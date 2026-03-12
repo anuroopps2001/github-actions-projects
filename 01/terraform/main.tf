85dc1e8b69144f123b10a76f2cf8f85b00987d24
Anuroop P S <anuroopps2001@gmail.com>
2026-03-12 22:18:40 +0530

    fix: pip sku

--- 01/terraform/main.tf
+++ 01/terraform/main.tf
@@ -36,6 +36,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "prometheus-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
   allocation_method   = "Static"
 }
 
