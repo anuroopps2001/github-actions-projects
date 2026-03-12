f4f52f5a03a4f1385377017cd483c293bc256666
Anuroop P S <anuroopps2001@gmail.com>
2026-03-12 20:48:46 +0530

    second commit

---
+++ 01/terraform/main.tf
@@ -0,0 +1,81 @@
+provider "azurerm" {
+  features {}
+}
+
+
+resource "tls_private_key" "vmss_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_private_key" {
+  content         = tls_private_key.vmss_key.private_key_pem
+  filename        = var.ssh_key_path
+  file_permission = "0600"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "prometheus-rg"
+  location = "centralindia"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "prometheus-vnt"
+  address_space       = ["11.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_subnet" "subnet" {
+  name                 = "prometheus-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["11.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "prometheus-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "prometheus-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                = "prometheus-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard"
+
+  admin_username = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.vmss_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ 01/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "ssh_key_path" {
+  default     = "./id_rsa.pem"
+  description = "Path to save ssh private key"
+  type        = string
+}
