b48582aea355b4f85894bcd1926c2381f7ee9d1f
Anuroop P S <anuroopps2001@gmail.com>
2026-03-12 22:07:11 +0530

    fix: remote backend

---
+++ 01/terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "anurooptfstate1773332278"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
